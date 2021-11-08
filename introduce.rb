5c760eb1652ef41583a4594ba0bfc9015c8105fd
yota <heyetech01@gmail.com>
2021-11-08 06:56:25 +0000

    修正

--- introduce.rb
+++
@@ -1,6 +0,0 @@
-myozi = '河野'
-namae = '陽太'
-puts myozi + namae + "です" + '。'
-num = 19
-tanni = '才'
-puts num.to_s + tanni + 'です' + '。'
---
+++ kadai-introduce.rb
@@ -0,0 +1,4 @@
+myozi = '河野'
+namae = '陽太'
+num = '19'
+puts myozi + namae + "です" + '。' + num.to_s + '才' + 'です' + '。'
